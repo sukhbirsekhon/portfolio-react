34ea09b3ae5cabd0f9031e476b29825989922bb5
sukhbirsekhon <sukhbirsekhon3939@gmail.com>
2020-10-27 17:02:46 -0400

    new updates

--- src/App.js
+++ src/App.js
@@ -48,7 +48,6 @@ class App extends Component {
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
         <Testimonials data={this.state.resumeData.testimonials}/>
         <Footer data={this.state.resumeData.main}/>
